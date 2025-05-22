b99f5bc6a79df0efdd805cade92784f7c197b833
Eliáš Jan Procházka <prochazkaeq@caliaq.dev>
2025-05-22 19:36:21 +0200

    feat: enhance error handling and update order and product schemas

--- src/models/order.js
+++ src/models/order.js
@@ -8,39 +8,37 @@ const orderSchema = new Schema(
     // visitorId: mongoId, required
     visitorId: {
       type: Schema.Types.ObjectId,
-      required: true,
       ref: "Visitor",
       validate: async (value) =>
         !!(await mongoose.model("Visitor").findById(value)),
     },
-    // productId: mongoId, required
-    productId: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "Product",
-      validate: async (value) =>
-        !!(await mongoose.model("Product").findById(value)),
-    },
+    // products array with productId and quantity
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+          validate: async (value) =>
+            !!(await mongoose.model("Product").findById(value)),
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          validate: (value) =>
+            validator.isNumeric(value.toString()) && value <= 99,
+        },
+        duration: {
+          type: Number,
+          validate: (value) => validator.isNumeric(value.toString()),
+        },
+      },
+    ],
     // startDate: date, required
-    startDate: {
+    date: {
       type: Date,
       required: true,
-      validate: {
-        validator: (value) => !isNaN(new Date(value).getTime()),
-        message: "Invalid startDate format",
-      },
-    },
-    // duration: number, required, numeric (in milliseconds)
-    duration: {
-      type: Number,
-      required: true,
-      validate: (value) => validator.isNumeric(value.toString()),
-    },
-    // quantity: number, required, numeric, max: 99
-    quantity: {
-      type: Number,
-      required: true,
-      validate: (value) => validator.isNumeric(value.toString()) && value <= 99,
+      validate: (value) => !isNaN(new Date(value).getTime()),
     },
   },
   // remove the version key
