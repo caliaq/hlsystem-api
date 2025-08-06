dd955d7233534862f663bde6aceb8b3ca41c3705
Eliáš Jan Procházka <prochazkae@caliaq.dev>
2025-08-06 22:24:21 +0200

    fix: update Dockerfile to use python:3.10-slim and streamline package installation; refactor main.py to remove gpiod dependency

--- gate-controller/main.py
+++ gate-controller/main.py
@@ -1,27 +1,26 @@
+import os
 from flask import Flask, jsonify
 
-import gpiod 
 import time
 
 app = Flask(__name__)
 
-chip = gpiod.Chip('gpiochip0')
-
 gate_open = False
+
+def write_to_pin(pin, value):
+    try: 
+        os.system(f"gpioset gpiochip0 {pin}={value}")
+    except Exception as e:
+        raise Exception(f"Failed to write to pin {pin}: {str(e)}")
 
 def toggle_gate():
     global gate_open
     try:
-
-        pin = chip.get_line(27)
-
-        pin.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-
-        pin.set_value(1)
+        write_to_pin(27, 1)
         print("on")
         time.sleep(0.5)
 
-        pin.set_value(0)
+        write_to_pin(27, 0)
         print("off")
 
         gate_open = not gate_open
